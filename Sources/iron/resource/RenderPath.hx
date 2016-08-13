6138d4455154c57f93f8e13a852677ee5e499436
Lubos Lenco <lubos.lenco@gmail.com>
2016-08-13 21:04:13 +0200

    Fix viewport calls.

--- Sources/iron/resource/RenderPath.hx
+++ Sources/iron/resource/RenderPath.hx
@@ -237,11 +237,8 @@ class RenderPath {
 		if (viewportScale != 1.0) {
 			var viewW = Std.int(currentRenderTargetW * viewportScale);
 			var viewH = Std.int(currentRenderTargetH * viewportScale);
-			#if js
 			currentRenderTarget.viewport(0, viewH, viewW, viewH);
-			#else
-			currentRenderTarget.viewport(0, 0, viewW, viewH);
-			#end
+			// currentRenderTarget.viewport(0, 0, viewW, viewH);
 		}
 		// else { // Set by Kha
 			// currentRenderTarget.viewport(0, 0, currentRenderTargetW, currentRenderTargetH);
