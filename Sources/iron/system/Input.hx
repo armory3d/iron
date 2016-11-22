5e4d3c9c2c634c8343efec05f0d5aee9ca0a5543
Lubos Lenco <lubos.lenco@gmail.com>
2016-11-22 22:17:03 +0100

    Store input movement.

--- Sources/iron/system/Input.hx
+++ Sources/iron/system/Input.hx
@@ -13,8 +13,8 @@ class Input {
 	public static var moved(default, null) = false;
 	public static var occupied = false;
 
-	public static var deltaX(default, null):Float = 0;
-	public static var deltaY(default, null):Float = 0;
+	public static var movementX(default, null):Float = 0;
+	public static var movementY(default, null):Float = 0;
 
 	public function new() {
 		kha.input.Mouse.get().notify(downListener, upListener, moveListener, null);
@@ -26,8 +26,8 @@ class Input {
 		started = false;
 		started2 = false;
 		moved = false;
-		deltaX = 0;
-		deltaY = 0;
+		movementX = 0;
+		movementY = 0;
 	}
 
 	public static function reset() {
@@ -39,8 +39,8 @@ class Input {
 		released2 = false;
 		moved = false;
 		occupied = false;
-		deltaX = 0;
-		deltaY = 0;
+		movementX = 0;
+		movementY = 0;
 	}
 	
 	public static function downListener(_index:Int, _x:Float, _y:Float) {
@@ -67,13 +67,13 @@ class Input {
 			released = true;
 		}
 
-		x = _x ;
-		y = _y ;
+		x = _x;
+		y = _y;
 	}
 	
-	public static function moveListener(_x:Int, _y:Int, movementX:Int, movementY:Int) {
-		deltaX = _x - x;
-		deltaY = _y - y;
+	public static function moveListener(_x:Int, _y:Int, _movementX:Int, _movementY:Int) {
+		movementX = _movementX;
+		movementY = _movementY;
 
 		x = _x ;
 		y = _y ;
