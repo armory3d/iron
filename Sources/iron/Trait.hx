5bf5f6bb4bf1c88e129e8d64f9b81ad5f0742c5f
Lubos Lenco <lubos.lenco@gmail.com>
2017-03-06 02:28:13 +0100

    Expose trait notify

--- Sources/iron/Trait.hx
+++ Sources/iron/Trait.hx
@@ -24,62 +24,62 @@ class Trait {
 		object.removeTrait(this);
 	}
 
-	function notifyOnAdd(f:Void->Void) {
+	public function notifyOnAdd(f:Void->Void) {
 		if (_add == null) _add = [];
 		_add.push(f);
 	}
 
-	function notifyOnInit(f:Void->Void) {
+	public function notifyOnInit(f:Void->Void) {
 		if (_init == null) _init = [];
 		_init.push(f);
 		App.notifyOnInit(f);
 	}
 
-	function notifyOnRemove(f:Void->Void) {
+	public function notifyOnRemove(f:Void->Void) {
 		if (_remove == null) _remove = [];
 		_remove.push(f);
 	}
 
-	function notifyOnUpdate(f:Void->Void) {
+	public function notifyOnUpdate(f:Void->Void) {
 		if (_update == null) _update = [];
 		_update.push(f);
 		App.notifyOnUpdate(f);
 	}
 
-	function removeUpdate(f:Void->Void) {
+	public function removeUpdate(f:Void->Void) {
 		_update.remove(f);
 		App.removeUpdate(f);
 	}
 	
-	function notifyOnLateUpdate(f:Void->Void) {
+	public function notifyOnLateUpdate(f:Void->Void) {
 		if (_lateUpdate == null) _lateUpdate = [];
 		_lateUpdate.push(f);
 		App.notifyOnLateUpdate(f);
 	}
 
-	function removeLateUpdate(f:Void->Void) {
+	public function removeLateUpdate(f:Void->Void) {
 		_lateUpdate.remove(f);
 		App.removeLateUpdate(f);
 	}
 
-	function notifyOnRender(f:kha.graphics4.Graphics->Void) {
+	public function notifyOnRender(f:kha.graphics4.Graphics->Void) {
 		if (_render == null) _render = [];
 		_render.push(f);
 		App.notifyOnRender(f);
 	}
 
-	function removeRender(f:kha.graphics4.Graphics->Void) {
+	public function removeRender(f:kha.graphics4.Graphics->Void) {
 		_render.remove(f);
 		App.removeRender(f);
 	}
 
-	function notifyOnRender2D(f:kha.graphics2.Graphics->Void) {
+	public function notifyOnRender2D(f:kha.graphics2.Graphics->Void) {
 		if (_render2D == null) _render2D = [];
 		_render2D.push(f);
 		App.notifyOnRender2D(f);
 	}
 
-	function removeRender2D(f:kha.graphics2.Graphics->Void) {
+	public function removeRender2D(f:kha.graphics2.Graphics->Void) {
 		_render2D.remove(f);
 		App.removeRender2D(f);
 	}
