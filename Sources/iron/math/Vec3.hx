e545d7080ee4a39cb6ab6dfc70a5d104ad2c315b
Martin Wallin <guzzard@gmail.com>
2017-12-07 12:03:57 +0100

    More vec2/3 sync

--- Sources/iron/math/Vec3.hx
+++ Sources/iron/math/Vec3.hx
@@ -11,7 +11,152 @@ class Vec3 {
 		this.z = z;
 	}
 
+	public function cross(v:Vec3):Vec3 {
+		var x2 = y * v.z - z * v.y;
+		var y2 = z * v.x - x * v.z;
+		var z2 = x * v.y - y * v.x;
+		x = x2;
+		y = y2;
+		z = z2;
+		return this;
+	}
+
+	public function crossvecs(a:Vec3, b:Vec3):Vec3 {
+		var x2 = a.y * b.z - a.z * b.y;
+		var y2 = a.z * b.x - a.x * b.z;
+		var z2 = a.x * b.y - a.y * b.x;
+		x = x2;
+		y = y2;
+		z = z2;
+		return this;
+	}
+
+	public function set(x:Float, y:Float, z:Float):Vec3{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		return this;
+	}
+
+	public function add(v:Vec3):Vec3 {
+		x += v.x;
+		y += v.y;
+		z += v.z;
+		return this;
+	}
+
+	public function addf(x:Float, y:Float, z:Float):Vec3 {
+		this.x += x;
+		this.y += y;
+		this.z += z;
+		return this;
+	}
+
+	public function addvecs(a:Vec3, b:Vec3):Vec3 {
+		x = a.x + b.x;
+		y = a.y + b.y;
+		z = a.z + b.z;
+		return this;
+	} 
+
+	public function subvecs(a:Vec3, b:Vec3):Vec3 {
+		x = a.x - b.x;
+		y = a.y - b.y;
+		z = a.z - b.z;
+		return this;
+	}
+
+	public function normalize():Vec3 {
+		var n = length();
+		if (n > 0.0) {
+			var invN = 1.0 / n;
+			this.x *= invN; this.y *= invN; this.z *= invN;
+		}
+		return this;
+	}
+
+	public function mult(f:Float):Vec3 {
+		x *= f; y *= f; z *= f;
+		return this;
+	}
+
+	public function dot(v:Vec3):Float {
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	public function setFrom(v:Vec3):Vec3 {
+		x = v.x; y = v.y; z = v.z;
+		return this;
+	}
+
+	public function clone():Vec3 {
+		return new Vec3(x, y, z);
+	}
+
+	public static function lerp(v1:Vec3, v2:Vec3, t:Float):Vec3 {
+		var target = new Vec3();
+		target.x = v2.x + (v1.x - v2.x) * t;
+		target.y = v2.y + (v1.y - v2.y) * t;
+		target.z = v2.z + (v1.z - v2.z) * t;
+		return target;
+	}
+
+	public function applyproj(m:Mat4):Vec3 {
+		var x = this.x; var y = this.y; var z = this.z;
+
+		// Perspective divide
+		var d = 1.0 / (m._03 * x + m._13 * y + m._23 * z + m._33);
+
+		this.x = (m._00 * x + m._10 * y + m._20 * z + m._30) * d;
+		this.y = (m._01 * x + m._11 * y + m._21 * z + m._31) * d;
+		this.z = (m._02 * x + m._12 * y + m._22 * z + m._32) * d;
+
+		return this;
+	}
+
+	public function applymat(m:Mat4):Vec3 {
+		var x = this.x; var y = this.y; var z = this.z;
+
+		this.x = m._00 * x + m._10 * y + m._20 * z + m._30;
+		this.y = m._01 * x + m._11 * y + m._21 * z + m._31;
+		this.z = m._02 * x + m._12 * y + m._22 * z + m._32;
+
+		return this;
+	}
+
+	public inline function equals(v:Vec3):Bool {
+		return x == v.x && y == v.y && z == v.z;
+	}
+
 	public inline function length() {
 		return Math.sqrt(x * x + y * y + z * z);
+	}
+
+	public function sub(v:Vec3):Vec3 {
+		x -= v.x; y -= v.y; z -= v.z;
+		return this;
+	}
+
+	public static inline function distance3d(v1:Vec3, v2:Vec3):Float {
+		return distance3df(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z);
+	}
+
+	public static inline function distance3df(v1x:Float, v1y:Float, v1z:Float, v2x:Float, v2y:Float, v2z:Float):Float {
+		var vx = v1x - v2x;
+		var vy = v1y - v2y;
+		var vz = v1z - v2z;
+		return Math.sqrt(vx * vx + vy * vy + vz * vz);
+	}
+
+	public function distanceTo(p:Vec3):Float {
+		return Math.sqrt((p.x - x) * (p.x - x) + (p.y - y) * (p.y - y) + (p.z - z) * (p.z - z));
+	}
+
+	public static function xAxis():Vec3 { return new Vec3(1.0, 0.0, 0.0); }
+	public static function yAxis():Vec3 { return new Vec3(0.0, 1.0, 0.0); }
+	public static function zAxis():Vec3 { return new Vec3(0.0, 0.0, 1.0); }
+
+	public function toString():String {
+		return "(" + this.x + ", " + this.y + ", " + this.z + ")";
 	}
 }
