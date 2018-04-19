3e2b6f8d89e5136801bd5dc43172127926847a83
luboslenco <lubos.lenco@gmail.com>
2018-04-19 16:27:50 +0200

    Expose ease functions

--- Sources/iron/system/Tween.hx
+++ Sources/iron/system/Tween.hx
@@ -131,31 +131,31 @@ class Tween {
 		}
 	}
 
-	static function easeLinear(k:Float):Float { return k; }
-	static function easeSineIn(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return 1 - Math.cos(k * Math.PI / 2); } }
-	static function easeSineOut(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return Math.sin(k * (Math.PI * 0.5)); } }
-	static function easeSineInOut(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return -0.5 * (Math.cos(Math.PI * k) - 1); } }
-	static function easeQuadIn(k:Float):Float { return k * k; }
-	static function easeQuadOut(k:Float):Float { return -k * (k - 2); }
-	static function easeQuadInOut(k:Float):Float { return (k < 0.5) ? 2 * k * k : -2 * ((k -= 1) * k) + 1; }
-	static function easeCubicIn(k:Float):Float { return k * k * k; }
-	static function easeCubicOut(k:Float):Float { return (k = k - 1) * k * k + 1; }
-	static function easeCubicInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * k * k * k : 0.5 * ((k -= 2) * k * k + 2); }
-	static function easeQuartIn(k:Float):Float { return (k *= k) * k; }
-	static function easeQuartOut(k:Float):Float { return 1 - (k = (k = k - 1) * k) * k; }
-	static function easeQuartInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * (k *= k) * k : -0.5 * ((k = (k -= 2) * k) * k - 2); }
-	static function easeQuintIn(k:Float):Float { return k * (k *= k) * k; }
-	static function easeQuintOut(k:Float):Float { return (k = k - 1) * (k *= k) * k + 1; }
-	static function easeQuintInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * k * (k *= k) * k : 0.5 * (k -= 2) * (k *= k) * k + 1; }
-	static function easeExpoIn(k:Float):Float { return k == 0 ? 0 : Math.pow(2, 10 * (k - 1)); }
-	static function easeExpoOut(k:Float):Float { return k == 1 ? 1 : (1 - Math.pow(2, -10 * k)); }
-	static function easeExpoInOut(k:Float):Float { if (k == 0) { return 0; } if (k == 1) { return 1; } if ((k /= 1 / 2.0) < 1.0) { return 0.5 * Math.pow(2, 10 * (k - 1)); } return 0.5 * (2 - Math.pow(2, -10 * --k)); }
-	static function easeCircIn(k:Float):Float { return -(Math.sqrt(1 - k * k) - 1); }
-	static function easeCircOut(k:Float):Float { return Math.sqrt(1 - (k - 1) * (k - 1)); }
-	static function easeCircInOut(k:Float):Float { return k <= .5 ? (Math.sqrt(1 - k * k * 4) - 1) / -2 : (Math.sqrt(1 - (k * 2 - 2) * (k * 2 - 2)) + 1) / 2; }
-	static function easeBackIn(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else { return k * k * ((DEFAULT_OVERSHOOT + 1) * k - DEFAULT_OVERSHOOT); } }
-	static function easeBackOut(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else { return ((k = k - 1) * k * ((DEFAULT_OVERSHOOT + 1) * k + DEFAULT_OVERSHOOT) + 1); } }
-	static function easeBackInOut(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else if ((k *= 2) < 1) { return (0.5 * (k * k * (((DEFAULT_OVERSHOOT * 1.525) + 1) * k - DEFAULT_OVERSHOOT * 1.525))); } else { return (0.5 * ((k -= 2) * k * (((DEFAULT_OVERSHOOT * 1.525) + 1) * k + DEFAULT_OVERSHOOT * 1.525) + 2)); } }
+	public static function easeLinear(k:Float):Float { return k; }
+	public static function easeSineIn(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return 1 - Math.cos(k * Math.PI / 2); } }
+	public static function easeSineOut(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return Math.sin(k * (Math.PI * 0.5)); } }
+	public static function easeSineInOut(k:Float):Float { if(k == 0){ return 0; } else if(k == 1){ return 1; } else { return -0.5 * (Math.cos(Math.PI * k) - 1); } }
+	public static function easeQuadIn(k:Float):Float { return k * k; }
+	public static function easeQuadOut(k:Float):Float { return -k * (k - 2); }
+	public static function easeQuadInOut(k:Float):Float { return (k < 0.5) ? 2 * k * k : -2 * ((k -= 1) * k) + 1; }
+	public static function easeCubicIn(k:Float):Float { return k * k * k; }
+	public static function easeCubicOut(k:Float):Float { return (k = k - 1) * k * k + 1; }
+	public static function easeCubicInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * k * k * k : 0.5 * ((k -= 2) * k * k + 2); }
+	public static function easeQuartIn(k:Float):Float { return (k *= k) * k; }
+	public static function easeQuartOut(k:Float):Float { return 1 - (k = (k = k - 1) * k) * k; }
+	public static function easeQuartInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * (k *= k) * k : -0.5 * ((k = (k -= 2) * k) * k - 2); }
+	public static function easeQuintIn(k:Float):Float { return k * (k *= k) * k; }
+	public static function easeQuintOut(k:Float):Float { return (k = k - 1) * (k *= k) * k + 1; }
+	public static function easeQuintInOut(k:Float):Float { return ((k *= 2) < 1) ? 0.5 * k * (k *= k) * k : 0.5 * (k -= 2) * (k *= k) * k + 1; }
+	public static function easeExpoIn(k:Float):Float { return k == 0 ? 0 : Math.pow(2, 10 * (k - 1)); }
+	public static function easeExpoOut(k:Float):Float { return k == 1 ? 1 : (1 - Math.pow(2, -10 * k)); }
+	public static function easeExpoInOut(k:Float):Float { if (k == 0) { return 0; } if (k == 1) { return 1; } if ((k /= 1 / 2.0) < 1.0) { return 0.5 * Math.pow(2, 10 * (k - 1)); } return 0.5 * (2 - Math.pow(2, -10 * --k)); }
+	public static function easeCircIn(k:Float):Float { return -(Math.sqrt(1 - k * k) - 1); }
+	public static function easeCircOut(k:Float):Float { return Math.sqrt(1 - (k - 1) * (k - 1)); }
+	public static function easeCircInOut(k:Float):Float { return k <= .5 ? (Math.sqrt(1 - k * k * 4) - 1) / -2 : (Math.sqrt(1 - (k * 2 - 2) * (k * 2 - 2)) + 1) / 2; }
+	public static function easeBackIn(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else { return k * k * ((DEFAULT_OVERSHOOT + 1) * k - DEFAULT_OVERSHOOT); } }
+	public static function easeBackOut(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else { return ((k = k - 1) * k * ((DEFAULT_OVERSHOOT + 1) * k + DEFAULT_OVERSHOOT) + 1); } }
+	public static function easeBackInOut(k:Float):Float { if (k == 0) { return 0; } else if (k == 1) { return 1; } else if ((k *= 2) < 1) { return (0.5 * (k * k * (((DEFAULT_OVERSHOOT * 1.525) + 1) * k - DEFAULT_OVERSHOOT * 1.525))); } else { return (0.5 * ((k -= 2) * k * (((DEFAULT_OVERSHOOT * 1.525) + 1) * k + DEFAULT_OVERSHOOT * 1.525) + 2)); } }
 	// elastic todo
 	// bounce todo
 }
