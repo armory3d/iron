4d80ee929fdab21126fd7090ecfab49712b31c22
unknown <lubos.lenco@gmail.com>
2019-04-06 11:06:09 +0200

    Bump exporter version

--- tools/io_export_arm.py
+++ tools/io_export_arm.py
@@ -16,7 +16,7 @@ bl_info = {
     "location": "File -> Export",
     "description": "Armory mesh data",
     "author": "Armory3D.org",
-    "version": (0, 6, 0),
+    "version": (0, 7, 0),
     "blender": (2, 80, 0),
     "wiki_url": "http://armory3d.org/iron",
     "tracker_url": "https://github.com/armory3d/iron/issues"
