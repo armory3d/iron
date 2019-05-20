ed1fbd6f0ce8413d6e30e1da0dc33bfe14e9a3a7
luboslenco <lubos.lenco@gmail.com>
2019-05-20 16:08:42 +0200

    Repeat render targets

--- Sources/iron/object/Uniforms.hx
+++ Sources/iron/object/Uniforms.hx
@@ -212,9 +212,9 @@ class Uniforms {
 					if (!paramsSet) {
 						// No filtering when sampling render targets
 						#if (rp_resolution_filter == "Point")
-						g.setTextureParameters(context.textureUnits[j], TextureAddressing.Clamp, TextureAddressing.Clamp, TextureFilter.PointFilter, TextureFilter.PointFilter, MipMapFilter.NoMipFilter);
+						g.setTextureParameters(context.textureUnits[j], TextureAddressing.Repeat, TextureAddressing.Repeat, TextureFilter.PointFilter, TextureFilter.PointFilter, MipMapFilter.NoMipFilter);
 						#else
-						g.setTextureParameters(context.textureUnits[j], TextureAddressing.Clamp, TextureAddressing.Clamp, TextureFilter.LinearFilter, TextureFilter.LinearFilter, MipMapFilter.NoMipFilter);
+						g.setTextureParameters(context.textureUnits[j], TextureAddressing.Repeat, TextureAddressing.Repeat, TextureFilter.LinearFilter, TextureFilter.LinearFilter, MipMapFilter.NoMipFilter);
 						#end
 						// context.paramsSet[j] = true;
 						paramsSet = true;
