93e4a2c6dd2df014e2076ebad0c155475f58ad83
tong <tong@disktree.net>
2020-06-16 18:12:17 +0200

    Use wasm api, add instantiateStreaming

--- Sources/iron/data/Wasm.hx
+++ Sources/iron/data/Wasm.hx
@@ -3,22 +3,29 @@ package iron.data;
 #if js
 
 class Wasm {
+
 	public var exports: Dynamic;
 
-	public static inline function instance(blob: kha.Blob, importObject: Dynamic = null): Wasm {
-		return new Wasm(blob, importObject);
+	public static function instance(blob: kha.Blob, importObject: Dynamic = null): Wasm {
+		var exports : Dynamic = (importObject == null) ? {} : importObject.exports;
+		return new Wasm(
+			new js.lib.webassembly.Instance(
+				new js.lib.webassembly.Module(blob.toBytes().getData()),
+				exports
+			).exports
+		);
 	}
 
-	function new(blob: kha.Blob, importObject: Dynamic) {
-		// Switch to WebAssembly.instantiateStreaming when available
-		var data = blob.toBytes().getData();
-		js.Syntax.code('var module = new WebAssembly.Module({0});', data);
-		if (importObject == null) {
-			js.Syntax.code('{0} = new WebAssembly.Instance(module).exports;', exports);
-		}
-		else {
-			js.Syntax.code('{0} = new WebAssembly.Instance(module, {1}).exports;', exports, importObject);
-		}
+	#if kha_html5_js
+	public static function instantiateStreaming(blob: kha.Blob, importObject: Dynamic = null, done: Wasm->Void) {
+		js.lib.WebAssembly.instantiateStreaming(new js.html.Response(blob.toBytes().getData(), {
+			headers: new js.html.Headers({"Content-Type": "application/wasm"})
+		} ), importObject ).then( m -> done(new Wasm(m.instance.exports)));
+	}
+	#end
+
+	function new( exports : Dynamic ) {
+		this.exports = exports;
 	}
 
 	public function getString(i: Int): String { // Retrieve string from memory pointer
@@ -28,16 +35,16 @@ class Wasm {
 		return s;
 	}
 
-	public function getMemory(offset: Int, length: Int): js.lib.Uint8Array {
-		return js.Syntax.code('new Uint8Array({0}.memory.buffer, {1}, {2});', exports, offset, length);
+	public inline function getMemory(offset: Int, length: Int): js.lib.Uint8Array {
+		return new js.lib.Uint8Array(exports.memory.buffer, offset, length);
 	}
 
-	public function getMemoryF32(offset: Int, length: Int): kha.arrays.Float32Array {
-		return js.Syntax.code('new Float32Array({0}.memory.buffer, {1}, {2});', exports, offset, length);
+	public inline function getMemoryF32(offset: Int, length: Int): kha.arrays.Float32Array {
+		return new kha.arrays.Float32Array(exports.memory.buffer).subarray( offset, length );
 	}
 
-	public function getMemoryU32(offset: Int, length: Int): kha.arrays.Uint32Array {
-		return js.Syntax.code('new Uint32Array({0}.memory.buffer, {1}, {2});', exports, offset, length);
+	public inline function getMemoryU32(offset: Int, length: Int): kha.arrays.Uint32Array {
+		return new kha.arrays.Uint32Array(exports.memory.buffer).subarray(offset, length);
 	}
 }
 
