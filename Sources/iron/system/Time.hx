cda39f7ba6e4141baf41b9be28f707a37b5aa72b
luboslenco <lubos.lenco@gmail.com>
2020-06-30 17:40:35 +0200

    Use display frequency

--- Sources/iron/system/Time.hx
+++ Sources/iron/system/Time.hx
@@ -3,18 +3,21 @@ package iron.system;
 class Time {
 
 	public static var step(get, never): Float;
-	static inline function get_step(): Float { return 1 / 60; }
+	static inline function get_step(): Float { return 1 / frequency; }
 
 	public static var scale = 1.0;
 	public static var delta(get, never): Float;
-	static inline function get_delta(): Float { return (1 / 60) * scale; }
+	static inline function get_delta(): Float { return (1 / frequency) * scale; }
 
 	static var last = 0.0;
 	public static var realDelta = 0.0;
 	public static inline function time(): Float { return kha.Scheduler.time(); }
 	public static inline function realTime(): Float { return kha.Scheduler.realTime(); }
 
+	static var frequency = 60;
+
 	public static function update() {
+		frequency = kha.Display.primary != null ? kha.Display.primary.frequency : 60;
 		realDelta = realTime() - last;
 		last = realTime();
 	}
