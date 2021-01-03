5e98c6ddad45fadf547896e7910802b3e3a86d3b
knowledgenude <phenriquefel@gmail.com>
2021-01-03 12:54:40 -0300

    temp-fix

--- Sources/iron/system/InputMap.hx
+++ Sources/iron/system/InputMap.hx
@@ -458,7 +458,7 @@ class InputAxisComponent {
 }
 
 class KeyboardAxisComponent extends InputAxisComponent {
-	final keyboard = Input.getKeyboard();
+	final keyboard: Keyboard = Input.getKeyboard();
 
 	public function new(parent: InputAxis, positiveKey: String, negativeKey: String): Void {
 		super(parent, positiveKey, negativeKey);
@@ -476,7 +476,7 @@ class KeyboardAxisComponent extends InputAxisComponent {
 }
 
 class MouseAxisComponent extends InputAxisComponent {
-	final mouse = Input.getMouse();
+	final mouse: Mouse = Input.getMouse();
 
 	public function new(parent: InputAxis, positiveKey: String, negativeKey: String): Void {
 		super(parent, positiveKey, negativeKey);
@@ -493,8 +493,8 @@ class MouseAxisComponent extends InputAxisComponent {
 		switch (positiveKey) {
 			case "moved x": if (movX > parent.deadzone) scale++;
 			case "moved y":	if (movY > parent.deadzone) scale++;
-			case "mouse movement x": if (movX > parent.deadzone) return movX - parent.deadzone;
-			case "mouse movement y": if (movY > parent.deadzone) return movY - parent.deadzone;
+			case "movement x": if (movX > parent.deadzone || movX < -parent.deadzone) return movX - parent.deadzone;
+			case "movement y": if (movY > parent.deadzone || movY < -parent.deadzone) return movY - parent.deadzone;
 			default: if (mouse.down(positiveKey)) scale++;
 		}
 		switch (negativeKey) {
@@ -525,18 +525,18 @@ class GamepadAxisComponent extends InputAxisComponent {
 		var leftMovY = gamepad.leftStick.movementY;
 
 		// Avoid division by zero
-		var rightTrigger = gamepad.down("r2") > 0.0 ? (gamepad.down("r2") - parent.pressure) / (1 - parent.pressure) : 0.0;
-		var leftTrigger = gamepad.down("l2") > 0.0 ? (gamepad.down("r2") - parent.pressure) / (1 - parent.pressure) : 0.0;
+		var rightTrigger = gamepad.down("r2") > 0.0 ? (gamepad.down("r2") - parent.pressure) / (1 - parent.pressure): 0.0;
+		var leftTrigger = gamepad.down("l2") > 0.0 ? (gamepad.down("r2") - parent.pressure) / (1 - parent.pressure): 0.0;
 
 		switch (positiveKey) {
 			case "right stick moved x": if (rightMovX > parent.deadzone) scale++;
 			case "right stick moved y": if (rightMovY > parent.deadzone) scale++;
-			case "right stick movement x": if (rightMovX > parent.deadzone) return rightMovX - parent.deadzone;
-			case "right stick movement y": if (rightMovY > parent.deadzone) return rightMovY - parent.deadzone;
+			case "right stick movement x": if (rightMovX > parent.deadzone || rightMovX < -parent.deadzone) return rightMovX - parent.deadzone;
+			case "right stick movement y": if (rightMovY > parent.deadzone || rightMovY < -parent.deadzone) return rightMovY - parent.deadzone;
 			case "left stick moved x": if (leftMovX > parent.deadzone) scale++;
 			case "left stick moved y": if (leftMovY > parent.deadzone) scale++;
-			case "left stick movement x": if (leftMovX > parent.deadzone) return leftMovX - parent.deadzone;
-			case "left stick movement y": if (leftMovY > parent.deadzone) return leftMovY - parent.deadzone;
+			case "left stick movement x": if (leftMovX > parent.deadzone || leftMovX < -parent.deadzone) return leftMovX - parent.deadzone;
+			case "left stick movement y": if (leftMovY > parent.deadzone || leftMovY < -parent.deadzone) return leftMovY - parent.deadzone;
 			case "right trigger": return rightTrigger;
 			case "left trigger": return leftTrigger;
 			default: if (gamepad.down(positiveKey) > parent.pressure) scale++;
