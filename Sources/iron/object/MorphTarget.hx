68a8847d1aab8b212c59623ac3640822163a6df3
QuantumCoderQC <QuantumCoderQC@gmail.com>
2021-10-14 21:21:46 +0200

    init morph target class

---
+++ Sources/iron/object/MorphTarget.hx
@@ -0,0 +1,22 @@
+package iron.object;
+
+import kha.arrays.Float32Array;
+import kha.FastFloat;
+import iron.data.Data;
+import iron.data.SceneFormat;
+
+class MorphTarget{
+
+    public var scaling: FastFloat;
+    public var offset: FastFloat;
+    public var data: TMorphTarget;
+    public var morphWeights: Float32Array;
+
+    public function new(){
+
+        
+    }
+
+
+
+}
