52074614f8612aa25ca4fcb7818c6b3cfcb161f3
luboslenco <lubos.lenco@gmail.com>
2023-01-19 14:00:37 +0100

    Use krom_wasm define

--- Sources/iron/data/ShaderData.hx
+++ Sources/iron/data/ShaderData.hx
@@ -27,7 +27,7 @@ class ShaderData {
 
 	#if (arm_noembed && kha_krom)
 	public static var shaderPath = "../krom-resources/";
-	public static inline var shaderExt = #if kha_vulkan ".spirv" #elseif (krom_android || krom_html5) ".essl" #elseif kha_opengl ".glsl" #elseif kha_metal ".metal" #else ".d3d11" #end ;
+	public static inline var shaderExt = #if kha_vulkan ".spirv" #elseif (krom_android || krom_wasm) ".essl" #elseif kha_opengl ".glsl" #elseif kha_metal ".metal" #else ".d3d11" #end ;
 	#end
 
 	public function new(raw: TShaderData, done: ShaderData->Void, overrideContext: TShaderOverride = null) {
